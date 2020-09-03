ef59c49fb815a1d9bccf7fb6bfae9ada677dbdcd
efekta <efekta9@gmail.com>
2020-09-03 11:33:24 +0300

    коучинг (+ адаптив)

--- build/js/common.js
+++ build/js/common.js
@@ -19,18 +19,44 @@ $(document).ready(function(){
     $('#company-link_1').on('click', function() {
         $('#company_2').removeClass('open');
         $('#company_3').removeClass('open');
-        $('#company_1').toggleClass('open')
+        $('#company_1').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
     });
     /**/
     $('#company-link_2').on('click', function() {
         $('#company_1').removeClass('open');
         $('#company_3').removeClass('open');
         $('#company_2').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
     });
     /**/
     $('#company-link_3').on('click', function() {
         $('#company_1').removeClass('open');
         $('#company_2').removeClass('open');
-        $('#company_3').toggleClass('open')
+        $('#company_3').toggleClass('open');
+        // $('.coaching-company-item').addClass('open');
+    });
+
+
+    /**/
+    $('#coaching-company-link_1').on('click', function() {
+        $('#coaching-company_2').removeClass('open');
+        $('#coaching-company_3').removeClass('open');
+        $('#coaching-company_1').toggleClass('open');
+        // $('#coaching-company-item_1').addClass('open');
+    });
+    /**/
+    $('#coaching-company-link_2').on('click', function() {
+        $('#coaching-company_1').removeClass('open');
+        $('#coaching-company_3').removeClass('open');
+        $('#coaching-company_2').toggleClass('open');
+        // $('#coaching-company-item_2').addClass('open');
+    });
+    /**/
+    $('#coaching-company-link_3').on('click', function() {
+        $('#coaching-company_1').removeClass('open');
+        $('#coaching-company_2').removeClass('open');
+        $('#coaching-company_3').toggleClass('open');
+        // $('#coaching-company-item_3').addClass('open');
     });
 });
